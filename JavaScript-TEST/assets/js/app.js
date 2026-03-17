c5aac96e554236a1efc3a74af27aa0409a8f4b80
shigyotakumu <takumu.shigyo200373@outlook.jp>
2026-03-17 09:54:34 +0900

    STUDY-JS
    jsの基本を学んだ

---
+++ JavaScript-TEST/assets/js/app.js
@@ -0,0 +1,56 @@
+let shigyo = "Hello world!!";
+shigyo = "Hello Word2";
+console.log(shigyo);
+
+const takumu = "He...hello World";
+console.log(takumu);
+
+let inoki = ["いち","にー","さん","ダーー‼"]
+console.log(inoki[3])
+
+// let index = 0;
+// while(index < inoki.length){
+//     console.log(inoki[index]);
+//     index++;
+// }
+
+if(inoki.length > 3){
+    console.log("ボンバイエ‼");
+} else {
+    console.log("ボンバ...!");
+}
+
+
+const test = (arg) => {
+    if(inoki.length > arg){
+    console.log("ボンバイエ‼");
+} else {
+    console.log("ボンバ...!");
+}
+};
+
+test(5);
+test(7);
+test(2);
+test(8);
+
+
+const test2 = {
+    color: "pink",
+    size: "large",
+    purfume: "mint",
+    bon: () => {
+        console.log("Hello BON!!");
+    }
+};
+console.log(test2.bon());
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
+// window.alert("Hello World!!)
+
+console.log(document);
+
+document.getElementsByTagName("button")[0].addEventListener("click", ()=> {
+    window.alert("Hello World!!!!")
+});
--- Python/practice_logic_ops.py/logic_ops_true.py
+++
@@ -1,5 +0,0 @@
-#if文と組み合わせ
-age = int(input("年齢: "))
-hasticket = True
-if age >= 18 and haskticet: print("入場できます")
-else: print("入場できません")
